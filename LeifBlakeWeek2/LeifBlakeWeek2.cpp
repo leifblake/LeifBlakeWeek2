35f6cb33a0fd43410656955e15bc384e7d46254f
Leif Blake <blake50@purdue.edu>
2023-09-01 15:52:32 -0400

    Initial code

--- LeifBlakeWeek2/LeifBlakeWeek2.cpp
+++ LeifBlakeWeek2/LeifBlakeWeek2.cpp
@@ -1,20 +1,21 @@
-// LeifBlakeWeek2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	float A;
+	float B;
+	float X;
+	cout << "Hi everyone. Lets do some math! We are going to solve: " << endl;
+	cout << "Ax + B = 0 " << endl;
+	cout << "for x! " << endl;
+	cout << "Please type a value for A, then hit enter: " << endl;
+	cin >> A;
+	cout << "Please type a value for B, then hit enter: " << endl;
+	cin >> B;
+	cout << "Solving" << A;
+	cout << "x+" << B;
+	cout << "=0 for x..." << endl;
+	X = -B / A;
+	cout << "The answer is:" << endl;
+	cout << X << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
